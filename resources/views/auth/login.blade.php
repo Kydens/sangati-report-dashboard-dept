33591712e6f7dc12dd8d311f7fb3bcc83c374653
Kydens <nathsyahed@gmail.com>
2024-06-08 18:05:28 +0700

    change: table export and set value login old

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,8 @@
                         @csrf
                         <div class="mb-3">
                             <label for="email" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="email" name="email" autofocus>
+                            <input type="email" class="form-control" id="email" name="email"
+                                value="{{ old('email') }}" autofocus>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
